c818b98e66ebf0fab6b921b7aaddb328d3b1107a
cabinetfortunatus <cafrecherches2@gmail.com>
2024-12-30 12:04:00 +0300

    second commit

--- back-office/src/pages/users.jsx
+++ back-office/src/pages/users.jsx
@@ -1,7 +1,60 @@
+import { useEffect, useState } from "react";
+import  {Axios} from  "../api/axios";
+import DataTable from "react-data-table-component";
 function Users(){
+    const axios =  Axios()
+    const [userData, setUserData] = useState([])
+    useEffect(() => {
+        const getUser = async () => {
+            let response = await axios.get('users')
+            .then((response) => {
+                console.log(response.data)
+                setUserData(response.data)
+                
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+            .finally(() => {
+               
+            })
+        }
+        getUser()
+    },[])
+    const columns = [
+        {
+            name: "ID",
+            selector: (row) => row.ID,
+        },
+        {
+            name: "Nom d'utilisateur",
+            selector: (row) => row.username,
+        },
+        {
+            name: "Mot de passe",
+            selector: (row) => row.password,
+        },
+    ];
+
+    const rows = [
+        userData.map((data) => {
+            [
+                {
+                    ID: data.id,
+                    username:data.username,
+                    password:data.password,
+                }
+            ]
+        })
+    ];
     return(<>
     
-        <div>Users</div>
+        <div>
+            <DataTable columns={columns} 
+            data={rows} 
+            fixedHeader
+            title="Admin/Utilisateurs" />
+        </div>
         
     </>)
     
