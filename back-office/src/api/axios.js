256974c5037a779bf2e035415c97266369306528
CabinetFortunatus <cafrecherches2@gmail.com>
2025-04-09 12:41:38 +0200

    hermann commit

--- back-office/src/api/axios.js
+++ back-office/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 export const Axios = () => {
 
     const Instance = axios.create({
-      baseURL: 'http://localhost:5001/'
+      baseURL: 'http://173.212.195.91:5001/'
       // headers: {
       //   'X-CSRFToken': getCsrfToken(),  
       //},
