c6d78bcb291e2da30f73f43b3d08bbaae45fa7ff
HermannRandri <hermannrandria8@gmail.com>
2025-05-14 05:00:51 +0200

    mobile app down

--- back-office/src/pages/login.jsx
+++ back-office/src/pages/login.jsx
@@ -82,6 +82,10 @@ return(<>
                     </div>
                     <div className='w-full my-4 text-sm flex items-center text-red-600 justify-center'>{error_msg}</div>
                     <button className='rounded-md bg-blue-500 px-4 py-2 w-32 mb-4 text-white font-semibold' onClick={handleLogin}>{isLogLoading ? (<PulseLoader size={10} color='#fff' />):'Se connecter' }</button>
+                    <a download href="/" className="flex w-full justify-center items-center text-sm gap-2 font-semibold text-gray-800 my-2 hover:underline" >
+                        Télécharger la version mobile ici
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" viewBox="0 0 32 32"><rect width={4} height={10} x={2} y={12} fill="#07A889" rx={2}></rect><rect width={4} height={10} x={26} y={12} fill="#07A889" rx={2}></rect><path fill="#07A889" d="M8 12h16v12H8zm2 12h4v4a2 2 0 0 1-2 2a2 2 0 0 1-2-2zm8 0h4v4a2 2 0 0 1-2 2a2 2 0 0 1-2-2zm3.545-19.759l2.12-2.12A1 1 0 0 0 22.251.707l-2.326 2.326a7.97 7.97 0 0 0-7.85 0L9.75.707a1 1 0 1 0-1.414 1.414l2.12 2.12A7.97 7.97 0 0 0 8 10h16a7.97 7.97 0 0 0-2.455-5.759M14 8h-2V6h2Zm6 0h-2V6h2Z"></path></svg>
+                    </a>
                 </form>
             </div>
             <div className='hidden md:flex items-center justify-center bg-white w-full h-full rounded-tr-xl rounded-br-xl'>
